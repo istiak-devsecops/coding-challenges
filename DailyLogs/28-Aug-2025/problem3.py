460437a990438b786c4fcad346a4ddefbee74878
Istiak <new.life3618@gmail.com>
2025-08-28 20:23:53 +0600

    Added comments

--- DailyLogs/28-Aug-2025/problem3.py
+++ DailyLogs/28-Aug-2025/problem3.py
@@ -1,6 +1,10 @@
-import re
+#extract email address fromt he text
 
+import re
 text = "Contact us at help@example.com or support@company.org"
 
-result = re.findall(r'[\w.]+@\w+\.\w+',text)
+#check for email address pattern
+result = re.findall(r'[\w.]+@\w+\.\w+',text) 
+
+
 print(result)
