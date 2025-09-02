8927185dfcf4c19930f6554bd4453766ffc3a45b
Istiak <new.life3618@gmail.com>
2025-09-02 15:17:52 +0600

    Build Task selection menue for the users

---
+++ MiniProject/text_analyzer.py
@@ -0,0 +1,15 @@
+# Text analyzer
+
+user_input = input("write few words here: ")
+
+# Selection menue for user
+while True:
+    print("Select which operations you wants to perform!")
+    print("1. Count characters, words, sentences")
+    print("2. Word frequency")
+    print("3. Longest/shortest word")
+    print("4. Top 5 common words")
+    print("5. Vowel & consonant count")
+    print("6. Exit")
+
+    break
