9c5738e7f6687e58246a0b478f2d28c4a3a6d0eb
Istiak <new.life3618@gmail.com>
2025-09-02 16:14:14 +0600

    Added character, word and sentence counter

--- MiniProject/text_analyzer.py
+++ MiniProject/text_analyzer.py
@@ -1,8 +1,19 @@
 # Text analyzer
-
 user_input = input("write few words here: ")
+words = []
 
-# Selection menue for user
+# Remove punctuation from the sentence for accurate counts
+import string
+for word in user_input.lower().split():
+    clean_word = ""
+    for char in word:
+        if char not in string.punctuation:
+            clean_word += char
+    words.append(clean_word)
+
+clean_sentence = " ".join(clean_text)
+
+# Selection menu for user
 while True:
     print("Select which operations you wants to perform!")
     print("1. Count characters, words, sentences")
@@ -11,5 +22,28 @@ while True:
     print("4. Top 5 common words")
     print("5. Vowel & consonant count")
     print("6. Exit")
+
+    choice = input("Select an option from the list by their number.")
+
+    import re
+    if choice == "1":
+        char_count = 0
+
+        #count total character
+        for chr in clean_sentence:
+            if chr != " ":
+                char_count += 1 
+        print(f"Total character count is: {char_count}")
+
+        #count total word
+        word_count = len(words)
+        print(f"Total word count is: {word_count}")
+
+        #Count sentence
+        search_pattern = r'[.?!](?=\s|$)'    
+        matches = re.findall(search_pattern,user_input)
+        print(f"Total number of sentences is: {len(matches)}")    
+
+
 
     break
