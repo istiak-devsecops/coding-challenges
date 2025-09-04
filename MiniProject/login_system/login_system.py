b859a1643c8f89cb7a569045d49f173fd9d62ceb
Istiak <new.life3618@gmail.com>
2025-09-04 23:03:56 +0600

    Added error handling

--- MiniProject/login_system/login_system.py
+++ MiniProject/login_system/login_system.py
@@ -1,27 +1,44 @@
 # Store a username + password inside your script. Ask the user to enter credentials.
 
 # Read userdata from the file
-with open("user_data.txt","r") as file:
-    for line in file:
-        line = line.strip()
-        if ":" in line:
-            username, passcode = line.split(":",1)
+try:
+    with open("user_data.txt","r") as file:
+        crediantials = []
+        for line in file:
+            line = line.strip()
+            if ":" in line:
+                try:
+                    username, passcode = line.split(":",1)
+                    crediantials.append((username,passcode))
+                except ValueError:
+                    print(f"Skipping malformed line...")
+except FileNotFoundError:
+    print(f"ERROR:File not found ...")
+    exit()
+if not crediantials:
+    print("No valid credintials found in the file.")
+    exit()
 
-max_attemps = 3
-attemp = 0
+max_attempts = 3
+attempt = 0
+authenticated = False
 
-while attemp < max_attemps:
+#login attemp loop
+while attempt < max_attempts:
     user = input("What is the username: ")
     password = input("What is the passcode: ")
 
-
-    if user == username and password == passcode:
-        print("Login successfull!")
+    for user, password in crediantials:
+        if user == username and password == passcode:
+            print("Login successfull!")
+            authenticated = True
+            break
+    if authenticated:
         break
     else:
-        attemp += 1
-        left = max_attemps - attemp
+        attempt += 1
+        left = max_attempts - attempt
         print(f"You have {left} attemp left.")
 
-if max_attemps == attemp:
+if not authenticated:
     print("Too many failed login attempt. Try again later.")
