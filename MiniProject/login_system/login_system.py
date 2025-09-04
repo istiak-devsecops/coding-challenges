daf833244d63473c61f6b5428b65ffe74c236635
Istiak <new.life3618@gmail.com>
2025-09-04 23:15:12 +0600

    HotFix: bug in the loop to check credintials where it was comparing empty variable

--- MiniProject/login_system/login_system.py
+++ MiniProject/login_system/login_system.py
@@ -2,20 +2,20 @@
 
 # Read userdata from the file
 try:
-    with open("user_data.txt","r") as file:
-        crediantials = []
+    with open("user_data.txt", "r") as file:
+        credentials = []
         for line in file:
             line = line.strip()
             if ":" in line:
                 try:
-                    username, passcode = line.split(":",1)
-                    crediantials.append((username,passcode))
+                    stored_user, stored_pass = line.split(":", 1)
+                    credentials.append((stored_user, stored_pass))
                 except ValueError:
                     print(f"Skipping malformed line...")
 except FileNotFoundError:
     print(f"ERROR:File not found ...")
     exit()
-if not crediantials:
+if not credentials:
     print("No valid credintials found in the file.")
     exit()
 
@@ -23,13 +23,13 @@ max_attempts = 3
 attempt = 0
 authenticated = False
 
-#login attemp loop
+# login attemp loop
 while attempt < max_attempts:
-    user = input("What is the username: ")
-    password = input("What is the passcode: ")
+    input_user = input("What is the username: ")
+    input_pass = input("What is the passcode: ")
 
-    for user, password in crediantials:
-        if user == username and password == passcode:
+    for user, password in credentials:
+        if input_user == user and password == password:
             print("Login successfull!")
             authenticated = True
             break
