f942a82245f42707fd87ccecbd2f1fd90165abb1
Istiak <new.life3618@gmail.com>
2025-09-06 22:45:01 +0600

    Now the function will show both the subarray range list and the max sum of the subarray

--- PatternBasedProblem/sliding_window.py
+++ PatternBasedProblem/sliding_window.py
@@ -10,8 +10,12 @@ sum_of_subarray = [] #append a list of the sum of sumarray
 
 while stop <= len(num_list):  #Loop will keep running till it doesn't exceed list lenght
         subarray = num_list[start:stop]
-        sum_of_subarray.append(sum(subarray))
+        sum_of_subarray.append((subarray, sum(subarray)))
         start += 1                              #move the index by one to the right
         stop = start + length_of_subarray
         
-print("Max sum of subarrary is:",max(sum_of_subarray))
+max_subarray = max(sum_of_subarray, key=lambda, x:x[1])
+max_subarray_trio, max_sum = max_subarray  # assaigning key and value to two new variable
+
+print(f"Max subarrary trio is: {max_subarray_trio}")
+print(f"Max subarray sum is: {max_sum}")
