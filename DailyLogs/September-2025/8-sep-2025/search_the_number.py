20258ca0e06a7b7be616eb96dfd70d0819b0aa99
Istiak <new.life3618@gmail.com>
2025-09-08 22:31:03 +0600

    Address comments and missing target response

--- DailyLogs/September-2025/8-sep-2025/search_the_number.py
+++ DailyLogs/September-2025/8-sep-2025/search_the_number.py
@@ -1,20 +1,25 @@
 nums = [1, 3, 5, 7, 9]
-target = 5
+target = 6
 
 
 start = 0
 end = len(nums) - 1
+not_found = True
 
 
 while start < end:
     mid = (start + end) // 2
     if nums[mid] > target:
-        end = mid - 1
+        end = mid - 1           #Move the left one slot left
     elif nums[mid] < target:
-        start = mid + 1
+        start = mid + 1         #Move the start one slot right
     else:
         print("Target found")
+        not_found = False   #Target found so break the loop
         break
+
+if not_found:       #Target not found so send the error messages
+    print("The target is not in the list!")
 
 
 
