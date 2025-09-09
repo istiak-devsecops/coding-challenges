9818ad27fe6213361eb846e95ad6e1756b56ae99
istiak-devsecops <istiak.ahmed.devsecops@gmail.com>
2025-09-09 20:02:34 +0600

    improve the variable naming

--- DailyLogs/September-2025/9-sep-2025/problem4.py
+++ DailyLogs/September-2025/9-sep-2025/problem4.py
@@ -4,15 +4,15 @@ k = 3
 start = 0
 stop = start + k
 
-sub_array_list = []
+total_sub_array_list = []
 
 while stop <= len(nums):
-    subarray_sum = nums[start:stop]
-    sub_array_list.append((subarray_sum, sum(subarray_sum)))
+    subarray_list = nums[start:stop]
+    total_sub_array_list.append((subarray_list, sum(subarray_list)))
     start += 1
     stop = start + k
 
-max_subarray = max(sub_array_list, key=lambda x:x[1])
+max_subarray = max(total_sub_array_list, key=lambda x:x[1])
 max_subarray_list, max_subarray_sum = max_subarray
 
 print(f"Max subarray List: {max_subarray_list} \nMax subarray sum: {max_subarray_sum}")
