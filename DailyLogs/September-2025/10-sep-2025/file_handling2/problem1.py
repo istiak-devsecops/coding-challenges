e7205ccdeb9b49f58bfddea205d3a4beaa2c0058
istiak-devsecops <istiak.ahmed.devsecops@gmail.com>
2025-09-10 20:39:39 +0600

    added error handling incase file is missing

--- DailyLogs/September-2025/10-sep-2025/file_handling2/problem1.py
+++ DailyLogs/September-2025/10-sep-2025/file_handling2/problem1.py
@@ -4,5 +4,13 @@ with open("notes.txt", "w") as file:
     file.write("Hello python.\nthis is the 2nd line.\nthis is the 3rd line.\nthis is the 4th line.\nthis is the 5th line.")
     file.close()
 
-with open("notes.txt", "r") as file:
-    print(file.read())
+with open("notes.txt", "a") as file:
+    file.write("\nThis is the 6th line.\nThis is the 7th line")
+
+try:
+    with open("notes.txt", 'r') as file:
+        for lines in file:
+            print(lines)
+
+except FileNotFoundError:
+    print("File not found")
