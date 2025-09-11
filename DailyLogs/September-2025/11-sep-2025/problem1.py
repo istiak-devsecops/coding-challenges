b4d4b5fdf3f6f12e60949bca9969af6708c0471e
Istiak <new.life3618@gmail.com>
2025-09-11 22:31:00 +0600

    Pass input to another variable list

---
+++ DailyLogs/September-2025/11-sep-2025/problem1.py
@@ -0,0 +1,26 @@
+# Write a function that accepts any number of numbers (*args) and returns the largest one.
+
+num1 = []
+num2 = []
+num3 = []
+
+lists = [num1,num2,num3]
+current_list = 0
+
+while True:
+    user_input = input("write a number or 'break': " )
+
+    if user_input.lower() == "break":
+        break
+
+    number = int(user_input)
+    if current_list < len(lists):
+        if len(lists[current_list]) < 3:
+            lists[current_list].append(number)
+        elif len(lists[current_list]) == 3:
+            current_list += 1
+    else:
+        print("All lines are full.")
+        break
+
+print(f"Num1: {num1}\nNum2: {num2}\nNum3: {num3}")
