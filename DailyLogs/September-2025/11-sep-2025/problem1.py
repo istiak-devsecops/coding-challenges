f26350f5749384a37f4c3dec3e33b3acb919d455
Istiak <new.life3618@gmail.com>
2025-09-11 23:02:07 +0600

    Added commensts and done some minor fix

--- DailyLogs/September-2025/11-sep-2025/problem1.py
+++ DailyLogs/September-2025/11-sep-2025/problem1.py
@@ -1,6 +1,6 @@
 # Write a function that accepts any number of numbers (*args) and returns the largest one.
 
-def find_largest_num(*args):
+def find_largest_num(*args):  #Take multiple variables as arguments
     return max(args)
 
 num1 = []
@@ -13,20 +13,20 @@ current_list = 0
 while True:
     user_input = input("write a number or 'break': " )
 
-    if user_input.lower() == "break":
+    if user_input.lower() == "break": #if the user call break the loop will break
         break
     
     try:
         number = int(user_input)
     except ValueError:
-        print("Write a valid positive number.")
+        print("Write a valid positive number.")  #error handling
         continue
     
-    if current_list < len(lists):
-        if len(lists[current_list]) < 3:
+    if current_list < len(lists):   #current list can not be higher than lenghth of the lists
+        if len(lists[current_list]) < 3:    #list will continue till the current_list is below 
             lists[current_list].append(number)
-        elif len(lists[current_list]) == 3:
-            current_list += 1
+        else:
+            current_list += 1   #start adding value to the next vaiable of the list
     else:
         print("All lines are full.")
         break
