56211391ab14057cc9732f65e204dfce624d2a68
Istiak <new.life3618@gmail.com>
2025-09-13 22:56:06 +0600

    func: word freq counter

---
+++ DailyLogs/September-2025/13-sep-2025/problem1.py
@@ -0,0 +1,24 @@
+# Write a function that takes a string and returns a dictionary with each word and how many times it appears.
+
+def word_counter():
+    note_book = {}
+    while True:
+        U_input = input("write a word('exit' to stop): ")
+        if U_input.lower().strip() == "exit":
+            break
+        
+        words = U_input.strip().split()
+        for word in words:
+            word = word.lower()
+            if word in note_book:
+                note_book[word] += 1
+            else:
+                note_book[word] = 1
+
+    return note_book
+
+word_freq = word_counter()
+print(word_freq)
+
+
+
