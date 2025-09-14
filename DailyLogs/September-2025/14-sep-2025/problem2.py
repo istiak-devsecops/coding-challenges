cbbcba6c863178a384f40a2109798986e639c907
Istiak <new.life3618@gmail.com>
2025-09-14 20:49:11 +0600

    added option for user to stop or continue the program

--- DailyLogs/September-2025/14-sep-2025/problem2.py
+++ DailyLogs/September-2025/14-sep-2025/problem2.py
@@ -12,8 +12,22 @@ def prime_checker(start,end):
 
     return prime_list
 
-start = int(input("Write the first number: "))
-end = int(input("Write the 2nd number: "))
+#taking input from user
 
-primes = prime_checker(start,end)
-print(f"The primes between {start} and {end} are: {primes}")
+
+while True:
+    #validing the range
+    start = int(input("Write the first number: "))
+    end = int(input("Write the 2nd number: "))
+    if start > end:
+        print("range should be in acending order(from low to high). Try again!")
+    else:
+        primes = prime_checker(start,end)
+        print(f"The primes between {start} and {end} are: {primes}")
+        user_input = input("You wants to continue or stop?Type 'y/n': ")
+        if user_input.lower() == "n":
+            break
+        else:
+            continue
+
+
