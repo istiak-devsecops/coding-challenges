4926e1dd1b55f04f3003b7c941dd9c2a55d4f7ce
Istiak <new.life3618@gmail.com>
2025-11-13 23:42:33 +0600

    Extract email script using bash with regex

--- DailyLogs/November-2025/12-nov/script3.sh
+++ DailyLogs/November-2025/12-nov/script3.sh
@@ -4,8 +4,19 @@
 
 TEXT="I love bash script. I like to play with bash script"
 
-if [[ "$TEXT" =~ ([a-z]+)\ bash ]]; then 
+if [[ $TEXT =~ ([a-z]+)\ bash ]]; then 
     echo "The word bash found"
 else 
+    echo "No match found"
+fi
+
+# extract all the valid email
+
+STRING="Hello Command, please reach out to us at support@devsecops.io for any assistance."
+REGEX="([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})"
+
+if [[ $STRING =~ $REGEX ]]; then
+    echo "Email: ${BASH_REMATCH[0]}" # - capture groups from your regex. $BASH_REMATCH[0] means entire match
+else
     echo "No match found"
 fi
