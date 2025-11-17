75c42fc9bec5b5b2f3b05e5cb8dc805a5d8804b9
Istiak <new.life3618@gmail.com>
2025-11-17 23:20:17 +0600

    script: created a .log file with script name and timestamp

---
+++ DailyLogs/November-2025/17-nov/script3/script3.py
@@ -0,0 +1,12 @@
+import sys
+import os
+from datetime import datetime
+
+script_name = os.path.basename(sys.argv[0])
+timestamp = datetime.now().strtftime("%Y-%m-%d_%H-%M-%S")
+log_file = f"{script_name}_{timestamp}.log"
+
+with open(log_file, "w")as file:
+    file.write(f"Log file created by {script_name} at {timestamp}")
+
+print(f"Log file created {log_file}")
