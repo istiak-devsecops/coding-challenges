9acb7cb5638c8f9cff00a9b2d2d09d64eb9c3811
Istiak <new.life3618@gmail.com>
2025-11-22 00:12:27 +0600

    scriptUpdate: change format of the code, make it more verbose, now it can take multiple commands as arguments

--- DailyLogs/November-2025/21-nov/script3/script3.py
+++ DailyLogs/November-2025/21-nov/script3/script3.py
@@ -6,11 +6,18 @@ if len(sys.argv) < 2:
     print("Usage: python3 script.py <command>")
     sys.exit(2) # invalid arguments
 
-shell_command = sys.argv[1]
+shell_command = " ".join(sys.argv[1])
 shell_command_results = subprocess.run(shell_command, capture_output=True,shell=True, text=True)
-timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_file = f"log_{timestamp}.txt"
 
-with open(f"log_{timestamp}.txt", "w")as file:
+with open(log_file, "w")as file:
+    file.write(f"Shell commands: {shell_command}\n")
+    file.write(f"Timestamp: {timestamp}\n")
     file.write("output log data: \n")
-    file.write(f"Output: {shell_command_results.stdout}\n")
-    file.write(f"Error: {shell_command_results.stderr}")
+    file.write("=====Standard Output=====\n")
+    file.write(shell_command_results.stdout if shell_command_results.stdout else "(no output)\n")
+    file.write("=====Standard Error=====\n")
+    file.write(shell_command_results.stderr if shell_command_results.stderr else "(no error)")
+
+print("Logged saved to: ",{log_file})
