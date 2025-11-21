0dcd4607f25b69b9b59a24d3dd8776b3c0463231
Istiak <new.life3618@gmail.com>
2025-11-21 23:52:14 +0600

    scriptFix: fixed wrong function in error handling, changed some variable name, removed os.path.join

--- DailyLogs/November-2025/21-nov/script2/script2.py
+++ DailyLogs/November-2025/21-nov/script2/script2.py
@@ -1,15 +1,18 @@
 import datetime
 import shutil
-import os
 import sys
 
 time_stamp = datetime.datetime.now().strftime("%Y-%m-%d")
 
 if len(sys.argv) < 2:
     print("Usage: python3 script.py <Directory name to archive>")
-    sys.argv(2) # invalid arguments
+    sys.exit(2) # invalid arguments
 
-dir_name = sys.argv[2]      # directory name from CLI
-archived_dir_name = os.path.join(f"{dir_name}_{time_stamp}")    # archived directory name
+dir_name = sys.argv[1]      # directory name from CLI
+archived_dir_name = f"{dir_name}_backup_{time_stamp}"    # archived directory name
 moved_file_to_dir = shutil.copytree(dir_name,archived_dir_name)  # copy whole dir to archive directory
-archive_file = shutil.make_archive(f"archive{time_stamp}","zip",archived_dir_name)  # compress the archive folder into archive.zip
+archive_path = shutil.make_archive(f"archive{time_stamp}","zip",dir_name)  # compress the archive folder into archive.zip
+
+
+print(f"Backup created at: {archived_dir_name}")
+print(f"Archive created at: {archive_path}")
