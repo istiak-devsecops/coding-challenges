0c5ee3a57c5727407ce7d99708661fc9153f0bb1
Istiak <new.life3618@gmail.com>
2025-11-23 23:29:41 +0600

    scriptUpdate: added dir path, walk through all the file and clean only the .tmp file

--- DailyLogs/November-2025/23-nov/script2/script2.py
+++ DailyLogs/November-2025/23-nov/script2/script2.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import shutil
 import logging
 
 logging.basicConfig(filename="backup.log",level=logging.DEBUG,format="%(asctime)s %(levelname)s: %(message)s")
@@ -9,3 +8,16 @@ if len(sys.argv) < 2:
     logging.error("Missing Arguments: ")
     print(f"Usage: python3 script.py <arguments>")
     sys.exit(2) # invalid arguments
+
+dir_name = sys.argv[1] #dir name
+dir_path = os.path.join(dir_name) # dir name
+logging.info("Cleaning process begun...")
+
+for root, dirs, files in os.walk(dir_path):
+    for file in files:
+        if file.endswith(".tmp"):
+            fullpath = os.path.join(root,file)
+            os.remove(fullpath)
+            logging.info(f"{fullpath} been removed successfully.")
+        else:
+            logging.error(f"Not possible to remvoe.")
