29adfefa8c2d097e9761955115ea0d95ac3594c4
Istiak <new.life3618@gmail.com>
2025-11-25 23:51:52 +0600

    scriptUpdate: print output with cli command, add= validate there are at least one key and one value

--- DailyLogs/November-2025/25-nov/script2/script2.py
+++ DailyLogs/November-2025/25-nov/script2/script2.py
@@ -1,3 +1,4 @@
+import sys
 import json
 from pathlib import Path
 
@@ -9,5 +10,22 @@ data = {
 
 setting_path = Path("settings.json")
 
+# stores the setting into the settings.json file
 with setting_path.open("w")as file:
     json.dump(data, file, indent=4)
+
+
+# opens the file to show output
+with setting_path.open()as file:
+    setting = json.load(file)
+
+
+# check if user only wants to print the current setting
+if  len(sys.argv) == 1:
+    print("Current settings: \n")
+    print(json.loads(setting, indent=4))
+    sys.exit(0) # success
+
+if len(sys.argv) != 3:
+    print("Usage: python3 script.py <key> <value>")
+    sys.exit(1) # exit code missing arguments
