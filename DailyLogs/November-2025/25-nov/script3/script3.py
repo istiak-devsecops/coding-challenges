8c2b9fa894bb21706597fb10a1db707d17d34430
Istiak <new.life3618@gmail.com>
2025-11-26 00:08:06 +0600

    scriptUpdate: log file to json

--- DailyLogs/November-2025/25-nov/script3/script3.py
+++ DailyLogs/November-2025/25-nov/script3/script3.py
@@ -11,5 +11,22 @@ if len(sys.argv) < 2:
 file_name = sys.argv[1]
 file_path = Path(file_name)
 
-with open(file_name, 'r')as file:
-    file.readlines()
+if not file_path.exists():
+    print("File doesn't exist.")
+    sys.exit(1) # exit code missing file
+
+lines = []
+
+with file_path.open('r')as file:
+    for i, line in enumerate(file, start=1)
+        lines.append({
+            "line": i,
+            "content": line,
+            "timestamp": datetime.datetime.now().isoformat()
+        })
+
+output_path = Path("output.json")
+with output_path.open('w')as file:
+    json.dump(line, file, indent=4)
+
+print(f"Saved {len(lines)} lines to '{output_path}'")
