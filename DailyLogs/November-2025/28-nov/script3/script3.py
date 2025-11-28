bf7cf4439bba7be084e576bb2e20f8e02e83be95
Istiak <new.life3618@gmail.com>
2025-11-28 23:30:21 +0600

    scriptUpdate: added main function to take CLI command and check service status

--- DailyLogs/November-2025/28-nov/script3/script3.py
+++ DailyLogs/November-2025/28-nov/script3/script3.py
@@ -2,10 +2,9 @@ import argparse
 import subprocess
 
 # function to check the service
-
 def check_service(service_name):
     try:
-        result = subprocess.run(["systemctl","status", service_name], capture_output=True, text=True)
+        result = subprocess.run(["systemctl","is-active", service_name], capture_output=True, text=True)
 
         status = result.stdout.strip()
 
@@ -20,4 +19,13 @@ def check_service(service_name):
     except Exception as e:
         print(f"Error: checking service {e}")
 
-        
+# main function to take CLI command"
+def main():
+    parser = argparse.ArgumentParser(description="check service status")
+    parser.add_argument("--service", required=True, help="Service name to check.")
+
+    args = parser.parse_args()
+    check_service(args.service)
+
+if __name__=="__main__":
+    main()
