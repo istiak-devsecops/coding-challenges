9f1f01bd04cc7a42af6ca53fefde75f84eda3a8b
Istiak <new.life3618@gmail.com>
2025-11-28 23:18:48 +0600

    scriptUpdate: added argparse to take an ip from CLI and validate

--- DailyLogs/November-2025/28-nov/script2/script2.py
+++ DailyLogs/November-2025/28-nov/script2/script2.py
@@ -1,5 +1,6 @@
 import argparse
 
+# function that validate a IPv4 address
 def valid_ipv4(ip):
     parts = ip.split(".")
 
@@ -17,4 +18,15 @@ def valid_ipv4(ip):
         
     return True
 
+# main function that takes user input from CLI and validate the ip
+def main():
+    parser = argparse.ArgumentParser(description="validate an IPv4 address.")
+    parser.add_argument("--ip",required=True, help="IPv4 address to validate.")
 
+    args = parser.parse_args()
+    ip = args.ip
+
+    if valid_ipv4(ip):
+        print(f"{ip} is a valid IPv4 address.")
+    else:
+        print(f"{ip} is NOT a valid IPv4 address.")
