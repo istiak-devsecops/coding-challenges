4f7070c68832f6ad6bb0891102c7ef62a915a830
Istiak <new.life3618@gmail.com>
2025-11-28 23:08:30 +0600

    scriptUpdate: log grepper with CLI commands

--- DailyLogs/November-2025/28-nov/script1/script1.py
+++ DailyLogs/November-2025/28-nov/script1/script1.py
@@ -1,10 +1,8 @@
 import argparse
 from pathlib import Path
 
-# define function  with two argument path and keyword
-# check if path exist
-# if exist open file and read line by line for keyword
 
+# function that will do the search operation
 def grep_file(file_path, keyword):
     path = Path(file_path)
 
@@ -20,3 +18,16 @@ def grep_file(file_path, keyword):
         for num, line in enumerate(file, start=1):
             if keyword.lower() in line.lower():
                 print(f"{num}: {line.rstrip()}")
+
+
+# main function that will take the command from CLI
+def main():
+    parser = argparse.ArgumentParser(description="search for a keyword inside a file.")
+    parser.add_argument("--file",required=True,metavar="Full File path", help="Path to the log file.")
+    parser.add_argument("--keyword",required=True,metavar="KEYWORD", help="Keyword to search for.")
+
+    args = parser.parse_args()
+    grep_file(args.file, args.keyword)
+
+if __name__=="__main__":
+    main()
