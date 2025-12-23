69d8a7258a636ed8d564e1bad6c2467563e84ee1
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2025-12-23 21:25:07 +0600

    scriptbash: user access simulation

--- DailyLogs/December-2025/23-dec/script2.sh
+++ DailyLogs/December-2025/23-dec/script2.sh
@@ -1,7 +1,17 @@
-# Reporting challenge with text highlighting
+# user access simulation
 
 #!/bin/bash
 
-red='\e[31m'
-green='\e[32m'
-reset='\e[0m'
+admin_user="root"
+admin_ip="192.168.1.1"
+
+read -p "Enter your username: " user
+read -p "Enter your ip address: " ip
+
+if [[ "$user" == "$admin_user" && "$ip" == "$admin_ip" ]]; then
+    echo "[ACCESS GRANTED]"
+elif [[ "$user" == "$admin_user"  "$ip" != "$admin_ip" ]]; then
+    echo "[SECURITY ALERT] Wrong IP for Root."
+else
+    echo "ACCESS DENIED"
+fi
