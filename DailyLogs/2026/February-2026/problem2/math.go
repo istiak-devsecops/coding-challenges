155ac3ea22b492de4870301dc54977533c3c8042
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2026-02-02 03:13:42 +0600

    Basic calculator

--- DailyLogs/2026/February-2026/problem2/math.go
+++ DailyLogs/2026/February-2026/problem2/math.go
@@ -9,22 +9,36 @@ import (
 // this is a comments
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go <num1> <num2>.")
+	if len(os.Args) < 4 {
+		fmt.Println("[Error] Usage: <num1> <num2> <add|sub|mul|div>.")
 		os.Exit(1)
 	}
 
-	numA, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Printf("[Error] '%s' is not a valid integer.\n", os.Args[1])
+	// type conversion of number
+	a, errA := strconv.Atoi(os.Args[1])
+	b, errB := strconv.Atoi(os.Args[2])
+	operations := os.Args[3]
+
+	if errA != nil || errB != nil {
+		fmt.Println("Please provide a valid integer.")
 		os.Exit(1)
 	}
 
-	numB, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		fmt.Printf("[Error] '%s' is not a valid integer.\n", os.Args[2])
+	switch operations {
+	case "add":
+		fmt.Printf("The sum of %d and %d is: %d\n", a, b, a+b)
+	case "sub":
+		fmt.Printf("The substraction of %d and %d is: %d\n", a, b, a-b)
+	case "mul":
+		fmt.Printf("The multiplication of %d and %d is: %d\n", a, b, a*b)
+	case "div":
+		if b == 0 {
+			fmt.Println("Go doesn't allow a number division with zero.")
+			os.Exit(1)
+		}
+		fmt.Printf("The division of %d and %d is: %d", a, b, a%b)
+	default:
+		fmt.Printf("%s is not a valid operations.", operations)
 		os.Exit(1)
 	}
-
-	fmt.Printf("The sum of %d and %d is: %d\n", numA, numB, numA+numB)
 }
