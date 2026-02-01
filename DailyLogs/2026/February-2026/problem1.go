7e73085ae072ddf284b0f549185e06056015f19d
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2026-02-02 01:07:42 +0600

    conditional logic in go -istiak

--- DailyLogs/2026/February-2026/problem1.go
+++ DailyLogs/2026/February-2026/problem1.go
@@ -5,13 +5,25 @@ import (
 	"os"
 )
 
-var name = "istiak"
+// var name = "istiak"
 
 func main() {
 
-	if len(os.Args) > 1 {
-		name = os.Args[1]
+	argCount := len(os.Args)
+
+	if argCount == 1 {
+		fmt.Println("Error: No name provided.")
+		fmt.Println("Usage: go run <filename.go> [FirstName] [LastName]")
+		os.Exit(1)
 	}
 
-	fmt.Printf("Hello, %s! Welcome to Go.\n", name)
+	if argCount == 2 {
+		fmt.Printf("Hello, %s! You forgot your [LastName]\n", os.Args[1])
+		fmt.Println("Usage: go run <filename.go> [FirstName] [LastName]")
+		os.Exit(1)
+	}
+
+	firstName := os.Args[1]
+	lastName := os.Args[2]
+	fmt.Printf("Hello, %s %s! Welcome to Go.\n", firstName, lastName)
 }
