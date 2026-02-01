45afc2c83e516cd3d79478455f2f369815b761ea
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2026-02-02 02:50:57 +0600

    error handling with go

--- DailyLogs/2026/February-2026/problem2/math.go
+++ DailyLogs/2026/February-2026/problem2/math.go
@@ -9,20 +9,22 @@ import (
 // this is a comments
 
 func main() {
-	switch len(os.Args) {
-	case 1:
-		fmt.Println("Error: I need two number to do sum.")
-		os.Exit(1)
-	case 2:
-		fmt.Println("Error: I need one more number to do sum.")
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run main.go <num1> <num2>.")
 		os.Exit(1)
 	}
 
-	A := os.Args[1]
-	B := os.Args[2]
+	numA, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("[Error] '%s' is not a valid integer.\n", os.Args[1])
+		os.Exit(1)
+	}
 
-	numA, _ := strconv.Atoi(A)
-	numB, _ := strconv.Atoi(B)
+	numB, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("[Error] '%s' is not a valid integer.\n", os.Args[2])
+		os.Exit(1)
+	}
 
-	fmt.Printf("The sum of %d and %d is: %d", numA, numB, numA+numB)
+	fmt.Printf("The sum of %d and %d is: %d\n", numA, numB, numA+numB)
 }
