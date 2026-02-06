00fbb4ddada6e560cd612f6ff20c1751fa080705
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2026-02-07 00:05:49 +0600

    multiple arguments from the CLI to check server status

--- DailyLogs/2026/February-2026/6-feb/problem1/statusChecker.go
+++ DailyLogs/2026/February-2026/6-feb/problem1/statusChecker.go
@@ -5,31 +5,34 @@ import (
 	"os"
 )
 
+// function to check hard coded server status
 func checkServer(name string) (status string, err error) {
-
-	if name == os.Args[1] {
+	if name == "web-server" {
 		return "Online", nil
 	}
 
-	return "", fmt.Errorf("%s is missing", name)
+	return "", fmt.Errorf("%s is missing.", name)
 }
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Please provide an arguments.")
+		fmt.Println("Please provide at least one arguments.")
 		os.Exit(1)
 	}
+	servers := os.Args[1:] //take multiple arguments
 
-	//slice of severs
-	servers := []string{"web-server", "db-server", "mail-server"}
+	for _, server := range servers {
+		status, err := checkServer(server)
 
-	//check if the arguments matches items from slice
-	for _, ser := range servers {
-		status, err := checkServer(ser)
 		if err != nil {
+			// if db-server is missing then exit the program
+			if server == "db-server" {
+				fmt.Println("CRITICAL ERROR: db-server is missing.")
+				return
+			}
 			fmt.Printf("Alert: %v\n", err)
 			continue
 		}
-		fmt.Printf("Health Check: %s is %s\n", ser, status)
+		fmt.Printf("Health check: %s is %s\n", server, status)
 	}
 }
