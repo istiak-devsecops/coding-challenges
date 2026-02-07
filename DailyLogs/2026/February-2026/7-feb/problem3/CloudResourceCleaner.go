8a21bccb39786eef088645ed8dd0ffc4fb2e3072
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2026-02-07 19:39:49 +0600

    interface with resources cleaner

---
+++ DailyLogs/2026/February-2026/7-feb/problem3/CloudResourceCleaner.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Resource interface {
+	Delete() error
+}
+
+type S3Bucket struct {
+	BucketName string
+}
+
+func (s S3Bucket) Delete() error {
+	fmt.Printf("Emptying and deleting bucket: %s\n", s.BucketName)
+	return nil
+}
+
+type ComputeInstance struct {
+	InstanceID string
+}
+
+func (c ComputeInstance) Delete() error {
+	fmt.Printf("Terminating instance: %s\n", c.InstanceID)
+	return nil
+}
+
+func CleanUp(r Resource) {
+	_ = r.Delete()
+}
+
+func main() {
+	bucket1 := S3Bucket{BucketName: "user-photos-prod"}
+	bucket2 := S3Bucket{BucketName: "temp-logs"}
+	server1 := ComputeInstance{InstanceID: "i-099abc123"}
+
+	resources1 := []Resource{bucket1, server1, bucket2}
+
+	for _, res := range resources1 {
+		CleanUp(res)
+	}
+}
