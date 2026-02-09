ad8b9c5692e6960817fb9d501731091d627a7225
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2026-02-09 10:42:14 +0600

    IDP Health orchestrator -istiak

---
+++ DailyLogs/2026/February-2026/9-feb/problem1/IDPHealthOrche.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+// Interface
+type Starter interface {
+	Start() error
+}
+
+type Checker interface {
+	Check() bool
+}
+
+type Resource interface {
+	Starter
+	Checker
+}
+
+// K8sApp Structs and Methods
+type K8sApp struct {
+	PodName string
+}
+
+func (k K8sApp) Start() error {
+	fmt.Printf("Scaling up pods for [%s]\n", k.PodName)
+	return nil
+}
+
+func (k K8sApp) Check() bool {
+	return true
+}
+
+// Database Structs and Methods
+type Database struct {
+	ConnString string
+}
+
+func (d Database) Start() error {
+	fmt.Printf("Opening connection to [%s]\n", d.ConnString)
+	return nil
+}
+
+func (d Database) Check() bool {
+	return false
+}
+
+// The Orchestrator(manager)
+func Reconcile(r Resource) {
+	r.Start()
+	if r.Check() {
+		fmt.Println("Resource is stable")
+	} else {
+		fmt.Println("CRITICAL: Resource is unhealthy!")
+	}
+}
+
+// main logic
+func main() {
+	kApp := K8sApp{PodName: "Test Pod1"}
+	dataB := Database{ConnString: "Test Database"}
+
+	Reconcile(kApp)
+	Reconcile(dataB)
+}
