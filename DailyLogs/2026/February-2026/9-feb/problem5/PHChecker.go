3c6caa6941d7538720b5fe1f8398a015cbbabf46
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2026-02-10 00:03:32 +0600

    using channel to strem data

--- DailyLogs/2026/February-2026/9-feb/problem5/PHChecker.go
+++ DailyLogs/2026/February-2026/9-feb/problem5/PHChecker.go
@@ -6,24 +6,32 @@ import (
 	"time"
 )
 
-func checkHealth(serviceName string, wg *sync.WaitGroup) {
+func checkHealth(serviceName string, wg *sync.WaitGroup, c chan string) {
 	defer wg.Done() // will execute before exiting the function
 
 	fmt.Printf("Checking [%s]...\n", serviceName)
 	time.Sleep(time.Second * 2)
-	fmt.Printf("%s is HEALTHY!\n", serviceName)
+	c <- fmt.Sprintf("%s is HEALTHY!", serviceName)
 }
 
 func main() {
 	var wg sync.WaitGroup // the counter
 
+	results := make(chan string, 3)
+
 	wg.Add(3) // added 3 task to the counter
 
-	go checkHealth("Database", &wg)
-	go checkHealth("Redis-Cache", &wg)
-	go checkHealth("Auth-Server", &wg)
+	go checkHealth("Database", &wg, results)
+	go checkHealth("Redis-Cache", &wg, results)
+	go checkHealth("Auth-Server", &wg, results)
 
 	wg.Wait()
+	close(results) // close the channel so the range loop knows it
+
+	fmt.Println("---Report Received---")
+	for msg := range results {
+		fmt.Println("Manager received:", msg)
+	}
 
 	fmt.Println("All the services ready. Starting IDP deployment...")
 }
