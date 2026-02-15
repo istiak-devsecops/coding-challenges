f4dbbb71ee6b9b05b25a3c5757a305096f4cc2ba
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2026-02-15 22:23:27 +0600

    Refactored the whole codebase

--- DailyLogs/2026/February-2026/15-feb/problem1/main.go
+++ DailyLogs/2026/February-2026/15-feb/problem1/main.go
@@ -8,29 +8,6 @@ import (
 	"sync"
 )
 
-type Provisioner interface {
-	Provision() string
-}
-
-// bluetprint for struct
-type Database struct{ Name string }
-type Server struct{ ID int }
-
-func (d Database) Provision() string { return fmt.Sprintf("Provisioning Database: %s\n", d.Name) }
-func (s Server) Provision() string   { return fmt.Sprintf("Provisioning Server: %d\n", s.ID) }
-
-// Data Transfer object
-type ProvisionRequest struct {
-	User       string   `json:"user"`
-	Components []string `json:"components"`
-}
-
-type ProvisionResponse struct {
-	Status  string   `json:"status"`
-	Message string   `json:"message"`
-	Results []string `json:"results"`
-}
-
 // The Logic Handler
 func httpHandler(w http.ResponseWriter, r *http.Request) {
 	// The gurad clause to check if the method is POST
