de82734d205dad92efa2fe6d0452a070a6a6d053
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2026-03-03 23:59:15 +0600

    added main function to the script

--- DailyLogs/2026/March-2026/3-mar/prb1/main.go
+++ DailyLogs/2026/March-2026/3-mar/prb1/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"sync"
 	"time"
 )
 
 type Result struct {
-	URL     []string
+	URL     string
 	Status  int
 	Latency time.Duration
 	Err     error
@@ -29,5 +30,35 @@ func checkService(url string, wg *sync.WaitGroup, results chan<- Result) {
 			return 0
 		}(),
 		Err: err,
+	}
+}
+
+func main() {
+	urls := []string{
+		"https://google.com",
+		"https://github.com",
+		"http://localhost:8080",
+	}
+
+	var wg sync.WaitGroup
+	results := make(chan Result, len(urls))
+
+	for _, u := range urls {
+		wg.Add(1)
+		go checkService(u, &wg, results)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for res := range results {
+		if res.Err != nil {
+			fmt.Printf("%s | Error: %v\n", res.URL, res.Err)
+
+		} else {
+			fmt.Printf("%s | Status: %d | Latency: %v\n", res.URL, res.Status, res.Latency)
+		}
 	}
 }
